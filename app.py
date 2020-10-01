6c0f5e6515ebcc37413083b62a5cdb4eb8c8f4f5
Max Slisarchuk <maksimslisarcuk@gmail.com>
2020-10-01 13:28:39 +0300

    make it better

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from selenium import webdriver
+
+op = webdriver.ChromeOptions()
+op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+op.add_argument("--headless")
+op.add_argument("--disable-dev-sh-usage")
+op.add_argument("--no-sandbox")
+
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=op)
+
+driver.get('http://www.google.com')
+print(driver.find_element_by_class_name("gNO89b").value)
+driver.quit()
