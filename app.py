d8c905cb7586f614130c6795f1487c7268b3b68f
Max Slisarchuk <maksimslisarcuk@gmail.com>
2020-11-17 14:32:06 +0200

    make it better

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ def csgomatches():
     op.add_argument('--incognito')
     op.add_argument('--proxy-server=%s' % PROXY)
 
-    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op, PORT=5000)
-    driver = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=op)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op, PORT=5000)
+    # driver = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=op)
 
     driver.get('https://www.hltv.org/')
     cont = driver.find_element_by_class_name("standard-list")
